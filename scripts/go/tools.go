4e7003146b52feb70ee53313015d3ebebea1f054
Disaiah Bennett <dbennett@redhat.com>
2023-03-01 09:53:35 -0500

    removed workaround for dockerfile.ocp
    Signed-off-by: Disaiah Bennett <dbennett@redhat.com>

--- scripts/go/tools.go
+++ scripts/go/tools.go
@@ -1,8 +1,11 @@
+//go:build tools
 // +build tools
 
 package main
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/google/subcommands"
+	_ "github.com/google/wire/cmd/wire"
 	_ "github.com/unknwon/bra"
 )
