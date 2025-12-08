ad4ff90745e70fae102c3d8f2f22511e298cecbd
Jacob Baungard Hansen <jacobbaungard@redhat.com>
2025-12-08 14:49:47 +0100

    assets: generate
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

---
+++ public/build/2228.05d90847c149cf83c0ec.js
@@ -0,0 +1,3 @@
+(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[2228],{32228:(()=>{ace.define("ace/snippets/text",["require","exports","module"],function(n,e,a){"use strict";e.snippetText=void 0,e.scope="text"})})}]);
+
+//# sourceMappingURL=2228.05d90847c149cf83c0ec.js.map
