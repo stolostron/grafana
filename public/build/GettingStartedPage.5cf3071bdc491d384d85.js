a3fb6402d53a00cc2f66cf08bacd2b30714ffaf9
Jacob Baungard Hansen <jacobbaungard@redhat.com>
2026-03-19 10:37:05 +0100

    assets: generate
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

---
+++ public/build/GettingStartedPage.5cf3071bdc491d384d85.js
@@ -0,0 +1,3 @@
+"use strict";(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[1037],{88552(s,e,a){s.exports=a.p+"static/img/provisioning.d25529e1.svg"}}]);
+
+//# sourceMappingURL=GettingStartedPage.5cf3071bdc491d384d85.js.map
--- public/build/PlaylistStartPage.8be462b13a24cddee188.js
+++
@@ -1,3 +0,0 @@
-"use strict";(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[1374],{93129:(s,l,a)=>{a.r(l),a.d(l,{default:()=>u});var t=a(2609);function u({match:n}){return t._o.start(n.params.uid),null}}}]);
-
-//# sourceMappingURL=PlaylistStartPage.8be462b13a24cddee188.js.map
