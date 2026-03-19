167ef633ddeaed894f5eb476d15c4bbe88a47572
Jacob Baungard Hansen <jacobbaungard@redhat.com>
2026-03-19 11:08:59 +0100

    assets: generate
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

---
+++ public/build/2223.1ebd9b7c16bcc533f4d3.js
@@ -0,0 +1,3 @@
+"use strict";(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[2223],{2223(u,a,n){n.r(a),n.d(a,{unstable:()=>s});const s={}}}]);
+
+//# sourceMappingURL=2223.1ebd9b7c16bcc533f4d3.js.map
--- public/build/explore-feature-toggle-page.cb1e2296c5e60ce26628.js
+++
@@ -1,5 +0,0 @@
-"use strict";(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[4422],{53518:(c,t,e)=>{e.r(t),e.d(t,{default:()=>r});var s=e(32196),n=e(96540),E=e(40845),l=e(67608);function o(a){return(0,s.css)({marginTop:a.spacing(2)})}function r(){const a=(0,E.of)(o);return n.createElement(l.Y,{className:a},n.createElement(l.Y.Contents,null,n.createElement("h1",null,"Explore is disabled"),"To enable Explore, enable it in the Grafana config:",n.createElement("div",null,n.createElement("pre",null,`[explore]
-enable = true
-`))))}}}]);
-
-//# sourceMappingURL=explore-feature-toggle-page.cb1e2296c5e60ce26628.js.map
