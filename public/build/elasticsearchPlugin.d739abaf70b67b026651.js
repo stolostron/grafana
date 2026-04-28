cf1d65e79dbfe301386d07bcae4ee1210417314d
Jacob Baungard Hansen <jacobbaungard@redhat.com>
2026-04-28 10:26:37 +0200

    assets: generate
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

---
+++ public/build/elasticsearchPlugin.d739abaf70b67b026651.js
@@ -0,0 +1,3 @@
+"use strict";(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[1717],{45622(a){a.exports=JSON.parse('{"id":"elasticsearch"}')}}]);
+
+//# sourceMappingURL=elasticsearchPlugin.d739abaf70b67b026651.js.map
